416a2d9fe5cf452620f85c3dbcd9fd2dcd36a142
Sergey <santerakis@gmail.com>
2022-12-18 20:23:34 +0200

    start template

---
+++ src/components/Site.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import {Navigate, Route, Routes} from 'react-router-dom';
+import styles from './Site.module.css'
+import {PageOne} from "./pages/PageOne";
+import {PageTwo} from "./pages/PageTwo";
+import {PageThree} from "./pages/PageThree";
+
+
+export const Site = () => {
+    return (
+        <div>
+            <div className={styles.header}><h1>HEADER</h1></div>
+            <div className={styles.body}>
+                <div className={styles.nav}>
+                    1234
+                </div>
+                <div className={styles.content}>
+                    <Routes>
+                        <Route path={'/'} element={ <Navigate to={'/page1'}/> }/>
+
+                        <Route path={'/page1'} element={<PageOne/>}/>
+                        <Route path={'/page2'} element={<PageTwo/>}/>
+                        <Route path={'/page3'} element={<PageThree/>}/>
+                        <Route/>
+                    </Routes>
+                </div>
+            </div>
+        </div>
+    );
+};
+
